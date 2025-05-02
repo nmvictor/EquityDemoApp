52bd1f93e6ac4dc47ce0b9bbc6f9dc85b5c36f15
Moin' Victor <victormn20@gmail.com>
2025-05-02 09:54:58 +0300

    Initial Commit

---
+++ EquityDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EquityDemoApp
+//
+//  Created by Moin' Victor on 02/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EquityDemoAppTests/EquityDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EquityDemoAppTests.swift
+//  EquityDemoAppTests
+//
+//  Created by Moin' Victor on 02/05/2025.
+//
+
+import Testing
+@testable import EquityDemoApp
+
+struct EquityDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
